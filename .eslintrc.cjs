3cad5dd95ed2ea1af1c96f9d85dbfba44d6fd541
Acceyuriko <Acceyuriko@gmail.com>
2022-08-02 22:45:36 +0800

    init commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    tsconfigRootDir: '.',
+    projects: ['./tsconfig.json'],
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['@typescript-eslint', 'react', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  root: true,
+  rules: {
+    'prettier/prettier': 'error',
+  },
+};
