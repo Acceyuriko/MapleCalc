2eada231ea90f68040764ce63b8e2e7ff2a901b1
Acceyuriko <Acceyuriko@gmail.com>
2022-12-02 21:58:48 +0800

    fix: use HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import 'antd/dist/antd.less';
 import App from './App';
 import './index.less';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter basename='MapleCalc'>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 );
