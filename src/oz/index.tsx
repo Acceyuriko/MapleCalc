3bfbede3cd28a11ded09685eea1ba5b44cc4bc84
Acceyuriko <Acceyuriko@gmail.com>
2023-04-16 11:33:30 +0800

    fix: map 48

--- src/oz/index.tsx
+++ src/oz/index.tsx
@@ -61,10 +61,10 @@ export const OZ = () => {
       height: 171,
     },
     map48: {
-      top: 64,
-      left: 26,
-      width: 278,
-      height: 126,
+      top: 37,
+      left: 10,
+      width: 258,
+      height: 152,
     },
     ...(localStorage.getItem(KEY_RECT_SETTINGS)
       ? JSON.parse(localStorage.getItem(KEY_RECT_SETTINGS)!)
